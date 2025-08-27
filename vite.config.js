8f4d3de60b4883f1461e5756d10cecdd213d0383
LucienPrabhat <ww28xx@gmail.com>
2025-08-27 14:14:39 +0800

    dev

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,6 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      // 代理捐款API請求
-      '/api/donate': {
-        target: 'https://www.children.org.tw',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/donate/, '/donate'),
-        headers: {
-          'Origin': 'https://www.youtube.com',
-          'Referer': 'https://www.youtube.com/'
-        }
-      }
-    }
-  }
+  plugins: [vue()]
 });
