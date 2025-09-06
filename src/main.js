6fe0510cfa2efaaa4057add7b4e2b3e40565f59a
LucienPrabhat <ww28xx@gmail.com>
2025-09-07 03:31:03 +0800

    fix: router issue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import App from "./App.vue";
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
@@ -50,7 +50,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
